4707f9715199754035d6a9163f9af7e0745ff30d
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-05-16 11:18:25 +0400

    constants and statics

---
+++ week_1/constants_and_statics/src/main.rs
@@ -0,0 +1,13 @@
+const MAX_PLAYYERS: u8 = 10;
+static CASINO_NAME:&str = "Rusty Casino";
+
+fn main() {
+    let a = MAX_PLAYYERS;
+    let b = MAX_PLAYYERS;
+
+    let c = CASINO_NAME;
+    let d = CASINO_NAME;
+
+}
+
+

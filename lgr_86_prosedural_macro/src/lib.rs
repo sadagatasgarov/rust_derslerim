192e0c8f31a0e7dbeb35f350e2b24cb1bee3cbb7
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-05-23 22:04:24 +0400

    lgr 88 prosedural macro Custom Derive

--- lgr_86_prosedural_macro/src/lib.rs
+++ lgr_86_prosedural_macro/src/lib.rs
@@ -1,13 +1,34 @@
-extern  crate proc_macro;
+extern crate proc_macro;
 
 use chrono::Utc;
-use proc_macro::{TokenStream};
+use proc_macro::TokenStream;
 use quote::quote;
 
-#[proc_macro]
-pub fn log_info(input: TokenStream) -> TokenStream{
-    let mut output = "[Info]".to_owned();
+#[proc_macro_derive(Log)]
+pub fn log_derive(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let trait_impl = quote! {
+        impl Log for #name {
+            fn info(&self, msg: &str) {
+                println!("[Info] {} {}", stringify!(#name), msg);
+            }
 
+            fn warn(&self, msg: &str) {
+                println!("[Warn] {} {}", stringify!(#name), msg);
+            }
+
+            fn error(&self, msg: &str) {
+                println!("[Error] {} {}", stringify!(#name), msg);
+            }
+        }
+    };
+    trait_impl.into()
+}
+
+#[proc_macro]
+pub fn log_info(input: TokenStream) -> TokenStream {
+    let mut output = "[Info]".to_owned();
 
     for token in input {
         let token_string = token.to_string();
@@ -17,7 +38,7 @@ pub fn log_info(input: TokenStream) -> TokenStream{
                 let time = Utc::now().time().to_string();
                 output.push_str(&format!(" {}", time));
             }
-            _=>{
+            _ => {
                 output.push_str(&format!(" {}", token_string));
             }
         }
--- lgr_86_prosedural_macro/src/main.rs
+++ lgr_86_prosedural_macro/src/main.rs
@@ -1,6 +1,39 @@
-use lgr_86_prosedural_macro::log_info;
+use lgr_86_prosedural_macro::*;
+
+trait Log {
+    fn info(&self, msg: &str);
+    fn warn(&self, msg: &str);
+    fn error(&self, msg: &str);
+}
+
+#[derive(Debug, Log)]
+struct Database {
+    url: String,
+    connections: u32,
+}
+
+impl Database {
+    fn new(url: String) -> Database {
+        Database {
+            url,
+            connections: 0,
+        }
+    }
+
+    fn connect(&mut self) {
+        self.info(format!("new connections to {}", self.url).as_str());
+        self.connections += 1;
+        if self.connections >= 100 {
+            self.warn(format!("100 or more connections open!").as_str());
+        }
+    }
+}
 
 fn main() {
-    //println!("Hello, world!");
+    let mut db = Database::new("localhost:5433".to_owned());
+    for _ in 0..100{
+        db.connect();
+    }
+
     log_info!([TIME] starting program...);
 }

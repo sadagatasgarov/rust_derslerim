d7a80843421420da186464373979cb802580293c
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-05-24 14:39:10 +0400

    cden rusu cagirmaq rustdan videodaki metod

--- lgr_97_FFI_C_from_rist/multiply/multiply.c
+++ lgr_97_FFI_C_from_rist/multiply/multiply.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int32_t multiply(int32_t a, int32_t b) {
+int multiply(int a, int b) {
     printf("[C] Hello from C!\n");
     printf("[C] Input a is: %i \n", a);
     printf("[C] Input b is: %i \n", b);
--- lgr_97_FFI_C_from_rist/src/main.rs
+++ lgr_97_FFI_C_from_rist/src/main.rs
@@ -1,3 +1,4 @@
+#[link(name = "multiply", kind="static")]
 extern "C" {
     fn multiply(a: i32, b: i32) -> i32;
 }
@@ -8,7 +9,7 @@ fn main() {
     unsafe {
         println!("[Rust] Calling function in C..");
 
-        let result = multiply(5000, 5);
+        let result = multiply(5i32, 5i32);
 
         println!("[Rust] Result: {}", result);
     }

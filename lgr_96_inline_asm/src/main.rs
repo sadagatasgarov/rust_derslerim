e0ecb55b17f3a062f3f43341c240f18c981b322f
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-05-24 13:16:18 +0400

    Inline asm

---
+++ lgr_96_inline_asm/src/main.rs
@@ -0,0 +1,19 @@
+use std::arch::asm;
+
+
+
+
+fn add(x: u64, y: u64) -> u64 {
+    let result: u64;
+    unsafe {
+        // x86 x86-64 asm
+        asm!("add {0}, {1}", inout(reg) x=> result, in(reg) y);
+
+    }
+    result
+}
+
+
+fn main() {
+    println!("{}", add(8, 9));
+}
